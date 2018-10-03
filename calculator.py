125508f5dafe5608274e329aea3f0cf3e3bcf775
Philipp Maksimov <filipp.39@gmail.com>
2018-10-03 18:43:06 +0200

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,19 @@
+import cmath
+import math
+from numbers import Real
+
+
+def radToDeg(rad: float):
+    return math.degrees(rad)
+
+
+def degToRad(deg: float):
+    return math.radians(deg)
+
+
+def arccos(rad: float):
+    return math.acos(rad)
+
+
+if __name__ == "__main__":
+    pass
